ce46c8cfa13c4ab009a0e99f8e714dd3a01e6180
Jardel Mandel Cardoso <jardel.mandel@gmail.com>
2023-01-16 17:56:58 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b= 1, c=0;
+
+            Console.Write("Quantos valores da sequencia você deseja que seja demostrado? ");
+            int valores = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sequencia de Fibonacci com " + valores + " valores");
+
+            for (int i = 0; i < valores; i++)
+            {
+                if (i < valores - 1)
+                {
+                    Console.Write(a + ", ");
+                }
+                else
+                {
+                    Console.Write(a);
+                }
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.ReadKey();
+                        
+
+        }
+    }
+}
